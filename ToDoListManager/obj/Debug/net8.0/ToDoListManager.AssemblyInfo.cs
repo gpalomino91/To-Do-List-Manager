8f3dfd31e75a00c6bf68248684de01cc113e0f46
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-03-03 19:02:39 +0000

    Added all projetc files

--- ToDoListManager/obj/Debug/net8.0/ToDoListManager.AssemblyInfo.cs
+++ ToDoListManager/obj/Debug/net8.0/ToDoListManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2238e96f314d2b6c5af829d8f4972be35ac93557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f72816d53cc99edf70b7faef4f7d83983b4d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
